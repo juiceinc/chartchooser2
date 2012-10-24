bfd91ca531f548f6d7a4f2b65b72209926e19cb1
Shalini Kataria <shalini.kataria@juiceanalytics.com>
2012-10-24 14:29:49 -0400

    Table component.

---
+++ assets/themes/twitter/js/libs/juice.table.js
@@ -0,0 +1,304 @@
+
+if (juice === undefined)
+  var juice = {};
+
+juice.table = function (conf) {
+  var _conf = conf || {},
+      grid = {},
+      slickgrid,
+      dataView,
+      sortcol,
+      data = _conf.data || [],
+      columns = _conf.columns || [],
+      options = _conf.gridOptions || {},
+      container = _conf.container || '#table',
+
+   formats = _conf.formats || {
+        SYMBOL_FLOAT:"d",
+        SYMBOL_INT:"i",
+        SYMBOL_CURRENCY:"$",
+        SYMBOL_PERCENT:"%",
+        SYMBOL_STRING:"s",
+        SYMBOL_NAN:"--"
+      };
+
+  init();
+
+  function init(){
+    dataView = new Slick.Data.DataView();
+    slickgrid = new Slick.Grid(container, dataView, columns, options);
+    slickgrid.setSelectionModel(new Slick.RowSelectionModel());
+
+    // wire up model events to drive the grid
+    dataView.onRowCountChanged.subscribe(function (e, args) {
+      slickgrid.updateRowCount();
+      slickgrid.render();
+
+      //console.log("current row count: "+args.current);
+    });
+
+    dataView.onRowsChanged.subscribe(function (e, args) {
+      slickgrid.invalidateRows(args.rows);
+      slickgrid.render();
+
+      //console.log("rows: "+args.rows);
+    });
+
+
+    //add event handlers
+    slickgrid.onSort.subscribe(function(e, args){ // args: sort information.
+      sortcol = args.sortCol.field;
+      dataView.sort(compare, args.sortAsc);
+    });
+
+    //initialize the model after all the events have been hooked up
+    dataView.beginUpdate();
+    dataView.setItems(data);
+    dataView.setFilterArgs({
+      filters: {}
+    });
+    dataView.setFilter(dataFilter);
+    dataView.endUpdate();
+
+
+    $('.leaderboard-table').on('mouseenter', '.slick-row', function(e){
+      $('.rank', this).show();
+    });
+
+     $('.leaderboard-table').on('mouseleave', '.slick-row', function(e){
+      $('.rank', this).hide();
+    });
+
+  }
+
+
+  /*function dataFilter(item, args) {
+    if(args && args.column !== "" && args.values !== ""){
+
+      //extract keys of item to see if there is any match for column
+      var cols = _.keys(item),
+          filterCol;
+
+      filterCol = _.find(cols, function(col){
+        return col.toLowerCase().indexOf(args.column.toLowerCase()) > -1;
+      });
+
+      if(filterCol){
+        var values = args.values.split(',');
+        values = _.without(values, "");
+        var match = _.find(values, function(val){
+          return item[filterCol].toLowerCase().indexOf($.trim(val).toLowerCase()) > -1
+        });
+      }
+
+      return match !== undefined;
+    }
+
+    return true;
+  }
+ */
+
+  //filter function for multiple column filters
+  function dataFilter(item, args){
+    var filters = args.filters,
+        columns = _.keys(filters);
+
+    if(filters && columns.length > 0){
+
+      for(var idx=0; idx<columns.length; idx+=1){
+        var currentFilterCol = columns[idx],
+            currentFilterValues = filters[currentFilterCol];
+
+        if(!currentFilterCol) return false;
+
+        if(currentFilterValues){
+          var values = currentFilterValues.split(',');
+          values = _.without(values, "");
+
+          var match = _.find(values, function(val){
+            return item[currentFilterCol].toLowerCase().indexOf($.trim(val).toLowerCase()) > -1;
+          });
+
+          if(match === undefined)
+            return;
+        }
+      }
+
+      return true;
+    }
+
+    return true;
+  }
+
+  grid.updateFilter = function(filters) {
+    dataView.setFilterArgs({filters: filters});
+    dataView.refresh();
+  };
+
+
+  grid.updateDisplay = function (conf){
+    if(conf && conf.columns && conf.columns.length > 0){
+      _.each(conf.columns, function(col){
+        if(col.format !== formats.SYMBOL_STRING)
+          col.formatter = customCellFormatter;
+      });
+
+      columns = conf.columns;
+    }
+
+    var f = conf.filters || {},
+        processedData = [];
+
+    if(conf && conf.data){
+      var d = conf.data;
+      data = calculateRanks(d);
+    }
+
+    dataView.beginUpdate();
+    dataView.setItems(data);
+    dataView.setFilterArgs({filters: f});
+    dataView.endUpdate();
+
+    slickgrid.setColumns(columns);
+
+    resetSortIndicator();
+
+    console.log("data after filter: "+dataView.getLength());
+  };
+
+  grid.getColumnByName = function(colStr){
+    if(!colStr) return;
+
+    if(data && data.length > 0){
+      var cols = _.keys(data[0]);
+
+      return _.find(cols, function(col){
+          return col.toLowerCase().indexOf(colStr.toLowerCase()) > -1;
+      });
+    }
+  };
+
+  //TODO: implement 'more is better' functionality, if needed
+  //is rank based on 'more is better' flag
+  function calculateRanks(data){
+    var processedData = data.slice();
+
+    _.each(columns, function(col){
+      if(col.format !== formats.SYMBOL_STRING){
+        var field = col.field;
+        //sort data by column
+        processedData = _.sortBy(processedData, function(d){
+                          return -d[field];
+                      });
+        //TODO: exclude null values from processed data?
+
+        //calculate ranks
+        var rank = 0,
+            currentVal,
+            prevVal,
+            rankField = col['field'] + '_rank';
+
+        for(var i=0; i<processedData.length; i+=1){
+          currentVal = processedData[i][field];
+          if(currentVal !== prevVal)
+            rank += 1;
+
+          processedData[i][rankField] = rank;
+          prevVal = currentVal;
+        }
+
+      }
+    });
+
+    //default sort by first numeric column
+    var pCol = _.find(columns, function(col){
+      return col.order !== -1;
+    });
+    var key = pCol && pCol.field ? pCol.field : "";
+
+    processedData = _.sortBy(processedData, function(row){
+      return -row[key];
+    });
+
+    return processedData;
+  }
+
+  function compare(a, b){
+    return a[sortcol] > b[sortcol] ? 1 : a[sortcol] < b[sortcol] ? -1 : 0;
+  }
+
+  function getFormattedValue(v, format){
+     if (isNaN(v))
+      return formats.SYMBOL_NAN;
+
+    var fm;
+
+    switch(format){
+      case formats.SYMBOL_INT:
+        fm = d3.format(",.0f")(v);
+        break;
+      case formats.SYMBOL_FLOAT:
+        fm = d3.format("0,.2f")(v);
+        break;
+      case formats.SYMBOL_PERCENT:
+        fm = d3.format("%")(v);
+        break;
+      case formats.SYMBOL_CURRENCY:
+        fm = '$' + d3.format("0,.0f")(v);
+        break;
+      default:
+        fm = v;
+        break;
+    }
+
+    return fm;
+  }
+
+  function getFormattedRank(rank){
+    var fmRank = "",
+        suffix = "th",
+        r = rank % 100;
+
+    if(r === 11 || r === 12 || r === 13)
+      return rank + suffix;
+
+    switch(rank % 10){
+      case 1:
+        suffix = "st";
+        break;
+      case 2:
+        suffix = "nd";
+        break;
+      case 3:
+        suffix = "rd";
+        break;
+    }
+
+    return rank + suffix;
+  }
+
+  function resetSortIndicator(){
+    //remove previous sort-icon if any
+    $('.slick-sort-indicator').removeClass('slick-sort-indicator-desc');
+    $('.slick-sort-indicator').removeClass('slick-sort-indicator-asc');
+
+    //update the 'sort indicator' for the default sort column (first numeric col)
+    var sortIndicators = $('.slick-header-column .slick-sort-indicator').not(':first'); //exclude the sort icon for the primary column
+    if(sortIndicators.length > 0){
+      var sortIcon = sortIndicators[0];
+      $(sortIcon).addClass('slick-sort-indicator-desc');
+    }
+  }
+
+  function customCellFormatter(row, cell, value, columnDef, dataContext) {
+    var field = columnDef.field,
+        rankField = field + '_rank',
+        fmValue = getFormattedValue(value, columnDef.format),
+        fmRank = getFormattedRank(dataContext[rankField]);
+
+    var cellContent = '<div class="cell-wrapper"><div class="rank">' + fmRank + '</div>' + '<div class="score">' + fmValue + '</div></div>';
+    return cellContent;
+  }
+
+  return grid;
+};
---
+++ assets/themes/twitter/js/table.js
@@ -0,0 +1,304 @@
+
+$(function(){
+  var data,
+      grid,
+      columns = [],
+      colWidth = 120,
+      FETCH_DATA_URL = 'http://chartchooser-files.s3-website-us-east-1.amazonaws.com/',
+      SAMPLE_DATA_HASH = '#358df037b92be0e2bdf069061275e570',
+      currentHash,
+
+      options = {
+        enableCellNavigation: true,
+        enableColumnReorder: false,
+        rowHeight: 30,
+        headerCssClass: "table-header"
+      },
+
+      appliedFilters = {},
+      autocomplete,
+      du = $(this).datautils();
+
+
+  //override the global data callback handler
+  onDataLoad = function(data){
+    //console.log("data successfully loaded...");
+    $('#csv-data').val(data.data);
+
+     if(data.reportTitle)
+      $('#report-title').text(data.reportTitle);
+
+    //update data
+    updateData();
+
+
+    /*
+    //set filters (should be applied AFTER the data is loaded, filters only work for applicable columns)
+    if(data.filters) {
+      _.each(data.filters, function(filterStr){
+        addFilterItem(filterStr);
+      });
+      filterData();
+    }   */
+  };
+
+  function updateData(){
+    resetFilters();
+
+    var csv = $('#csv-data').val();
+    data = d3.csv.parse(csv);
+
+    //add an id attribute to each row, required by slickgrid
+    _.each(data, function(row, idx){
+      row.id = "id_" + idx;
+    });
+
+    var columns = du.datautils('findTableColumns', data);
+
+    if(columns.length > 0){
+      //console.log("num of cols: "+columns.length);
+      var cWidth = columns[0].minWidth || colWidth;
+      $('#table').css('width', cWidth * columns.length + 20);
+    }
+
+    refreshTable({"data": data,
+                  "columns": columns,
+                  "filters": appliedFilters
+                  });
+  }
+
+  function refreshTable(conf){
+    grid.updateDisplay(conf);
+  }
+
+
+  function resetFilters(){
+    appliedFilters = {};
+
+    $('#filter-grid').val("");
+    $('.dataFilter').remove();
+  }
+
+
+  function loadData(){
+
+    if(window.location.hash === '' || window.location.hash === '#')
+      window.location.hash = SAMPLE_DATA_HASH;
+
+    currentHash = window.location.hash;
+
+    var hash = window.location.hash.replace('#', '');
+    //data url is located under a separate folder that starts with the first letter of the hash
+    //eg: 358df037b92be0e2bdf069061275e570 is stored under 3/358df037b92be0e2bdf069061275e570
+    var dataURL = hash.substr(0,1) + '/' + hash;
+
+    //onDataLoad will get triggered from JSONP
+    $.ajax({
+      type: "GET",
+      url: FETCH_DATA_URL + dataURL,
+      dataType : "jsonp",
+      jsonp: false,
+      error: function(err) { }
+    });
+  }
+
+  function addHashChangeHandler() {
+
+    if ('onhashchange' in window) {
+      $(window).bind('hashchange', checkUrl);
+    }
+    else
+      _checkUrlInterval = setInterval(checkUrl, 50 /*ms interval*/);
+  }
+
+  function checkUrl(e) {
+
+    if (currentHash == window.location.hash)
+      return false;
+    else loadData();
+  }
+
+
+  function filterData(columnName, values){
+    var appFilters = appliedFilters,
+        currentFilters = _.extend({}, appFilters);
+
+    var colFilter = grid.getColumnByName(columnName),
+        appliedColVal = currentFilters[colFilter] || "";
+
+    currentFilters[colFilter] = values + ',' +  appliedColVal;
+
+    grid.updateFilter(currentFilters);
+  }
+
+
+  function updateAppliedFilters(columnName, values){
+    var colFilter = grid.getColumnByName(columnName);
+
+    if(_.has(appliedFilters, colFilter) && appliedFilters[colFilter]){
+      var appVals = appliedFilters[colFilter];
+
+      var valsArray = values.split(','),
+          appValsArray = appVals.split(',');
+
+      var newVals = _.union(valsArray, appValsArray).join();
+      appliedFilters[colFilter] = newVals;
+    }else {
+      appliedFilters[colFilter] = values;
+    }
+
+    grid.updateFilter(appliedFilters);
+  }
+
+
+  function showAppliedFilters(){
+    $('.dataFilter').remove();
+    var curFilters = _.keys(appliedFilters);
+
+    for(var idx=0; idx < curFilters.length; idx+=1){
+     var newFilter = '<div class="alert alert-info fade in dataFilter" data-column="' + curFilters[idx] + '">' +
+                      '<button class="close remove-filter" data-dismiss="alert">&times</button>' +
+                        curFilters[idx] + " : " + appliedFilters[curFilters[idx]] +
+                    '</div>';
+
+      $('#applied-filters').append(newFilter);
+    }
+
+    $('#filter-grid').val("");
+  }
+
+
+  function start(){
+    addHashChangeHandler();
+    $('#filter-form').submit(function (e) { showAppliedFilters(); return false; });
+    $('#filter-form').on('click', '.remove-filter', function(e){
+      var col = $(this).parent().attr('data-column');
+      delete appliedFilters[col];
+      grid.updateFilter(appliedFilters);
+    });
+
+    $('#csv-data').keyup(function(e) {
+        // Arrow keys
+        if (e.keyCode != 39 && e.keyCode != 37 && e.keyCode != 40 && e.keyCode != 38) {
+            $('#clear-data-btn').addClass('btn-info');
+            updateData();
+        }
+    });
+
+    $('#clear-data-btn').click(function() {
+        $('#clear-data-btn').removeClass('btn-info');
+        loadData();
+        return false; //prevent refresh
+    });
+
+    $('#filter-grid').change(function(e){
+      var filter = $.trim($(this).val()),
+           searchStr = "";
+
+      if(filter.length > 0 && filter.indexOf(':') === -1){
+        filter += ' : ';
+        $(this).val(filter);
+      }else {
+        //text contains col and values, update the grid
+        searchStr = filter.split(':');
+        if(searchStr.length === 2)
+        {
+          var columnName = $.trim(searchStr[0]),
+              values = $.trim(searchStr[1]);
+
+          updateAppliedFilters(columnName, values);
+        }
+      }
+    });
+
+    //grab the autocomplete options and update the grid
+    $("#filter-grid").keyup(function (e) {
+      var filterText = this.value,
+           cols = [],
+           searchStr = filterText.split(':');
+
+      // clear on Esc
+      if (e.which == 27) {
+        this.value = "";
+      }
+
+      //return on enter, change event handler will take care of updating the grid
+      if(e.which == 13){
+         return;
+      }
+
+      if(data && data.length > 0)
+        cols = _.keys(data[0]);
+
+       autocomplete.data("typeahead").source =  function(){
+        if(filterText.indexOf(':') > -1){
+          var filterColName = $.trim(searchStr[0]),
+              filterColValueStr = searchStr.length > 1 ? $.trim(searchStr[1]).toLowerCase() : "";
+
+          var filterColValues = filterColValueStr.split(',');
+
+          var colValues = _.uniq(_.pluck(data, filterColName));
+
+          return _.filter(colValues, function(val){
+            if(val === undefined) return false;
+
+            return _.any(filterColValues, function(cv){
+              return val.toLowerCase().indexOf($.trim(cv).toLowerCase()) > -1;
+            });
+          });
+
+        }else {
+          return _.filter(cols, function(col){
+            return col.toLowerCase().indexOf(filterText.toLowerCase()) > -1;
+          });
+        }
+      };
+
+      if(searchStr.length !== 2)
+        return;
+
+      var columnName = $.trim(searchStr[0]),
+          values = $.trim(searchStr[1]);
+
+      if(values) filterData(columnName, values);
+
+    });
+
+
+    grid = juice.table({
+      data: data,
+      columns: columns,
+      gridOptions: options
+    });
+
+    autocomplete = $('#filter-grid').typeahead({
+      items: 20,
+      matcher: function(item){
+        return item;
+      },
+      updater: function(item){
+        var filter = $('#filter-grid').val(),
+            col,
+            vals = "";
+
+        if(filter.indexOf(':') > -1){
+          var str = filter.split(':');
+
+          col = $.trim(str[0]);
+          if(str.length > 1 && str[1].indexOf(',') > -1){
+            vals = $.trim(str[1]);
+            vals = vals.substring(0, vals.lastIndexOf(',') + 1);
+          }
+
+          item = col + ' : ' + vals + item;
+        }
+        return item;
+      }
+    });
+
+    loadData();
+  }
+
+  start();
+
+});
